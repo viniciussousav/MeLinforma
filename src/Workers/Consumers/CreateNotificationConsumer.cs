444ebbc05d25eaff95eaaf6735f159740ca99fe4
viniciussousav <vsv@cin.ufpe.br>
2023-08-23 13:58:01 -0300

    [test]: Sync Remote

--- src/Workers/Consumers/CreateNotificationConsumer.cs
+++ src/Workers/Consumers/CreateNotificationConsumer.cs
@@ -6,6 +6,6 @@ public class CreateNotificationConsumer : BackgroundService
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         Console.WriteLine("Hello World");
-        await Task.Delay(100000, stoppingToken);
+        await Task.Delay(100000, stoppingToken); 
     }
 }
